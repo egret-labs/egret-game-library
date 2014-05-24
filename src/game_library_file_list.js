bd9b2e64e78273bbaee11c0be5ecdaf59265a31f
DOM <dom@domlib.com>
2014-05-25 04:26:32 +0800

    完成Resource资源加载库

---
+++ src/game_library_file_list.js
@@ -0,0 +1,14 @@
+var game_library_file_list = [
+	//RES start
+    "resource/events/ResourceEvent.js",
+    "resource/core/ResourceItem.js",
+    "resource/core/ResourceConfig.js",
+    "resource/core/ResourceLoader.js",
+    "resource/analyzer/AnalyzerBase.js",
+    "resource/analyzer/BinAnalyzer.js",
+    "resource/analyzer/ImgAnalyzer.js",
+    "resource/analyzer/JsonAnalyzer.js",
+    "resource/analyzer/TxtAnalyzer.js",
+    "resource/Resource.js"
+    //RES end
+]
