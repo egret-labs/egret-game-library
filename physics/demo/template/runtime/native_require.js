aaec7f8aef205ad3a405d1ca695fd3b2d6190c70
zhangyu <runinspring@gmail.com>
2015-09-24 21:56:05 +0800

    upgrade to 2.5

---
+++ physics/demo/template/runtime/native_require.js
@@ -0,0 +1,66 @@
+
+var game_file_list = [
+    //以下为自动修改，请勿修改
+    //----auto game_file_list start----
+	"libs/modules/egret/egret.js"
+	
+	,"libs/modules/egret/egret.native.js"
+	
+	,"libs/modules/game/game.js"
+	
+	,"libs/modules/game/game.native.js"
+	
+	,"libs/modules/physics/physics.js"
+	
+	,"libs/modules/res/res.js"
+	
+	,"bin-debug/LoadingUI.js"
+	
+	,"bin-debug/Main.js"
+	//----auto game_file_list end----
+];
+
+var window = {};
+
+egret_native.setSearchPaths([""]);
+
+egret_native.requireFiles = function () {
+    for (var key in game_file_list) {
+        var src = game_file_list[key];
+        require(src);
+    }
+};
+
+egret_native.egretInit = function () {
+    egret_native.requireFiles();
+    egret.TextField.default_fontFamily = "/system/fonts/DroidSansFallback.ttf";
+    //egret.dom为空实现
+    egret.dom = {};
+    egret.dom.drawAsCanvas = function () {
+    };
+};
+
+egret_native.egretStart = function () {
+    var option = {
+        //以下为自动修改，请勿修改
+        //----auto option start----
+entryClassName: "Main",
+		frameRate: 30,
+		scaleMode: "showAll",
+		contentWidth: 480,
+		contentHeight: 800,
+		showPaintRect: false,
+		showFPS: false,
+		fpsStyles: "x:0,y:0,size:30",
+		showLog: false,
+		logFilter: "",
+		maxTouches: 2,
+		textureScaleFactor: 1
+//----auto option end----
+    };
+
+    egret.native.NativePlayer.option = option;
+    egret.runEgret();
+    egret_native.Label.createLabel(egret.TextField.default_fontFamily, 20, "", 0);
+    egret_native.EGTView.preSetOffScreenBufferEnable(true);
+};
