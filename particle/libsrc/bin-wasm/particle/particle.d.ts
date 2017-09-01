2e146ece99628fed06ea748ba14e4ac37d46cc68
himuil <jerry.lin@egret.com>
2017-09-01 13:50:11 +0800

    Add wasm lib

---
+++ particle/libsrc/bin-wasm/particle/particle.d.ts
@@ -0,0 +1,225 @@
+declare namespace particle {
+    class ParticleSystem extends egret.DisplayObject {
+        /**
+         * @private
+         */
+        protected _emitterX: number;
+        protected _emitterY: number;
+        private _emitterBounds;
+        /**
+         * 相对当前显示对象坐标系下的内容边界
+         * @private
+         */
+        protected relativeContentBounds: egret.Rectangle;
+        /**
+         * 表示粒子出现总时间，单位毫秒，取值范围(0,Number.MAX_VALUE]，-1表示无限时间
+         * @member {number} particle.ParticleSystem#emissionTime
+         * @default -1
+         */
+        emissionTime: number;
+        /**
+         * 表示粒子出现间隔，单位毫秒，取值范围(0,Number.MAX_VALUE]
+         * @member {number} particle.ParticleSystem#emissionRate
+         */
+        emissionRate: number;
+        /**
+         * 表示粒子所使用的纹理
+         * @member {egret.Texture} particle.ParticleSystem#texture
+         */
+        texture: egret.Texture;
+        /**
+         * 表示粒子系统最大粒子数，超过该数量将不会继续创建粒子，取值范围[1,Number.MAX_VALUE]
+         * @member {number} particle.ParticleSystem#maxParticles
+         * @default 200
+         */
+        maxParticles: number;
+        /**
+         * 表示粒子类，如果设置创建粒子时将创建该类
+         * @member {number} particle.ParticleSystem#particleClass
+         */
+        particleClass: any;
+        $particleConfig: any;
+        constructor(texture: egret.Texture, emissionRate: number);
+        initConfig(emissionRate: number, emitterX: number, emitterY: number): void;
+        protected createWebAssemblyNode(): void;
+        /**
+         * 开始创建粒子
+         * @param duration {number} 粒子出现总时间
+         */
+        start(duration?: number): void;
+        stop(clear?: boolean): void;
+        setCurrentParticles(num: number): void;
+        onPropertyChanges(): void;
+        /**
+         * 表示粒子出现点X坐标，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.ParticleSystem#emitterX
+         * @default 0
+         */
+        emitterX: number;
+        /**
+         * 表示粒子出现点Y坐标，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.ParticleSystem#emitterY
+         * @default 0
+         */
+        emitterY: number;
+        /**
+         * 更新当前显示对象坐标系下的边框界限
+         * @param emitterRect {egret.Rectangle} 相对发射点坐标系下的界限
+         */
+        private updateRelativeBounds(emitterRect);
+        /**
+         * 表示当前粒子系统中发射粒子的渲染边界范围，使用以发射点为基准的坐标系
+         * @member {egret.Rectangle} particle.ParticleSystem#emitterBounds
+         */
+        emitterBounds: egret.Rectangle;
+        changeTexture(texture: egret.Texture): void;
+    }
+}
+declare namespace particle {
+    class GravityParticleSystem extends ParticleSystem {
+        /**
+         * 表示粒子初始坐标 x 差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#emitterXVariance
+         */
+        private emitterXVariance;
+        /**
+         * 表示粒子初始坐标 y 差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#emitterYVariance
+         */
+        private emitterYVariance;
+        /**
+         * 表示粒子存活时间，单位毫秒，取值范围(0,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#lifespan
+         */
+        private lifespan;
+        /**
+         * 表示粒子存活时间差值，单位毫秒，取值范围(0,Number.MAX_VALUE]且不大于 lifespan
+         * @member {number} particle.GravityParticleSystem#lifespanVariance
+         */
+        private lifespanVariance;
+        /**
+         * 表示粒子出现时大小，取值范围(0,Number.MAX_VALUE]，粒子将会在存活时间内由 startSize 慢慢变为 endSize
+         * @member {number} particle.GravityParticleSystem#startSize
+         */
+        private startSize;
+        /**
+         * 表示粒子出现时大小差值，取值范围(0,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#startSizeVariance
+         */
+        private startSizeVariance;
+        /**
+         * 表示粒子消失时大小，取值范围(0,Number.MAX_VALUE]，粒子将会在存活时间内由 startSize慢慢变为 endSize
+         * @member {number} particle.GravityParticleSystem#endSize
+         */
+        private endSize;
+        /**
+         * 表示粒子消失时大小差值，取值范围(0,Number.MAX_VALUE]，且不大于endSize
+         * @member {number} particle.GravityParticleSystem#endSizeVariance
+         */
+        private endSizeVariance;
+        /**
+         * 表示粒子出现时的角度，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#emitAngle
+         */
+        private emitAngle;
+        /**
+         * 表示粒子出现时的角度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#emitAngleVariance
+         */
+        private emitAngleVariance;
+        /**
+         * 表示粒子出现时旋转值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]，粒子将会在存活时间内由 startRotation 慢慢变为 endRotation
+         * @member {number} particle.GravityParticleSystem#startRotation
+         */
+        private startRotation;
+        /**
+         * 表示粒子出现时旋转值差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#startRotationVariance
+         */
+        private startRotationVariance;
+        /**
+         * 表示粒子消失时旋转值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]，粒子将会在存活时间内由 startRotation 慢慢变为 endRotation
+         * @member {number} particle.GravityParticleSystem#endRotation
+         */
+        private endRotation;
+        /**
+         * 表示粒子消失时旋转值差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#endRotationVariance
+         */
+        private endRotationVariance;
+        /**
+         * 表示粒子出现时速度，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#speed
+         */
+        private speed;
+        /**
+         * 表示粒子出现时速度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#speedVariance
+         */
+        private speedVariance;
+        /**
+         * 表示粒子水平重力，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#gravityX
+         */
+        private gravityX;
+        /**
+         * 表示粒子垂直重力，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#gravityX
+         */
+        private gravityY;
+        /**
+         * 表示粒子径向加速度，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#radialAcceleration
+         */
+        private radialAcceleration;
+        /**
+         * 表示粒子径向加速度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#radialAccelerationVariance
+         */
+        private radialAccelerationVariance;
+        /**
+         * 表示粒子切向加速度，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#tangentialAcceleration
+         */
+        private tangentialAcceleration;
+        /**
+         * 表示粒子切向加速度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#tangentialAccelerationVariance
+         */
+        private tangentialAccelerationVariance;
+        /**
+         * 表示粒子出现时的 Alpha 透明度值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]，粒子将会在存活时间内由 startAlpha 慢慢变为 endAlpha
+         * @member {number} particle.GravityParticleSystem#startAlpha
+         */
+        private startAlpha;
+        /**
+         * 表示粒子出现时的 Alpha 透明度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#startAlphaVariance
+         */
+        private startAlphaVariance;
+        /**
+         * 表示粒子消失时的 Alpha 透明度值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]，粒子将会在存活时间内由 startAlpha 慢慢变为 endAlpha
+         * @member {number} particle.GravityParticleSystem#endAlpha
+         */
+        private endAlpha;
+        /**
+         * 表示粒子消失时的 Alpha 透明度差值，取值范围[-Number.MAX_VALUE,Number.MAX_VALUE]
+         * @member {number} particle.GravityParticleSystem#endAlphaVariance
+         */
+        private endAlphaVariance;
+        /**
+         * 表示粒子使用的混合模式
+         * @member {number} particle.GravityParticleSystem#blendMode
+         */
+        private particleBlendMode;
+        /**
+         * 是否完成解析json数据
+         */
+        private $init;
+        constructor(texture: egret.Texture, config: any);
+        start(duration?: number): void;
+        setCurrentParticles(num: number): void;
+        onPropertyChanges(): void;
+        private $parseConfig(config);
+    }
+}
