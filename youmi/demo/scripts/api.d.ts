492ab5c438d6e9822cd1323666f45022f68d25eb
himuil <jerry.lin@egret.com>
2018-05-07 12:27:14 +0800

    Upgrade to 5.1.11

--- youmi/demo/scripts/api.d.ts
+++ youmi/demo/scripts/api.d.ts
@@ -81,6 +81,18 @@ type ProjectConfig = {
     showLog: boolean;
     maxTouches: number;
 }
+/**
+   * 匹配机制，将满足 from 的文件输出为 to 格式的文件
+   * from 采用 glob 表达式 , to 包含 [path][name][hash][ext]四个变量
+   * 示例：{ from:"resource/**.*" , to:"[path][name]_[hash].[ext]" }
+   */
+type Matcher = {
+
+    from: string[],
+
+    to: string
+
+}
 
 
 declare namespace plugins {
@@ -243,8 +255,9 @@ declare module 'built-in' {
      * * contents : 将 EXML 的内容写入到主题文件中
      * * gjs : 将生成的JS文件写入到主题文件中
      * * commonjs : 将EXML合并为一个 CommonJS 风格的文件
+     * * commonjs2 : 将EXML合并为一个含有解析方法和皮肤定义的文件，且皮肤抽离为一份配置
      */
-    type EXML_Publish_Policy = "default" | "debug" | "contents" | "gjs" | "commonjs"
+    type EXML_Publish_Policy = "default" | "debug" | "contents" | "gjs" | "commonjs" | "commonjs2"
 
 
 
@@ -327,23 +340,47 @@ declare module 'built-in' {
         constructor(options: CleanPluginOptions);
     }
 
+
     type RenamePluginOptions = {
 
+        /**
+         * 是否输出日志
+         */
         verbose?: boolean
 
+        /**
+         * 采用何种 hash 算法，目前暂时只支持 crc32
+         */
         hash?: "crc32"
 
-        matchers: { from: string, to: string }[]
+
+        /**
+         * 设置匹配规则，将指定文件进行改名
+         * 该参数是个数组，允许设置多个匹配规则
+         */
+        matchers: Matcher[]
     }
+
+
+    /**
+     * 修改文件名插件
+     */
     export class RenamePlugin implements plugins.Command {
         constructor(options: RenamePluginOptions);
     }
 
     type ResSplitPluginOptions = {
 
+        /**
+         * 是否输出日志
+         */
         verbose?: boolean
 
-        matchers: { from: string, to: string }[]
+        /**
+         * 设置匹配规则，将指定文件拷贝至其他文件夹
+         * 该参数是个数组，允许设置多个匹配规则
+         */
+        matchers: Matcher[]
     }
 
     export class ResSplitPlugin implements plugins.Command {
